f2795ad366a9913baef2d3f576ad78ab01de8cfc
madvirus-ops <edwinayabie1@gmail.com>
2022-09-17 20:29:53 +0100

    docs and test

--- ApiBlog/settings.py
+++ ApiBlog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views 
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
+
+
+
+
+schema_view = get_swagger_view(title='Post API')
+
 app_name = 'core'
 urlpatterns = [
    # path('',views.home, name='home'),
@@ -13,5 +22,8 @@ urlpatterns = [
     path('create',views.ApiGenerics.as_view(),name='create'),
     path('update',views.ApiGenerics.as_view(),name='update'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='detail'),
-    path('list',views.postlist,name='list')
+    path('list',views.postlist,name='list'),
+    path('docs/',schema_view),
+    path('api/docs/', include_docs_urls(title='Posts API')),
+
 ]
