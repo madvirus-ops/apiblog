18eb2c01075bd46aa70afae21df0b0bbc56457a8
madvirus-ops <edwinayabie1@gmail.com>
2022-10-02 18:58:20 +0100

    streaks, profile form

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 
+from dataclasses import fields
 from django import forms
-from .models import sendMaill,Comment
+from .models import sendMaill,Comment,Profile
 
 
 class sendmaill(forms.ModelForm):
@@ -13,3 +14,7 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('name','email','body')
         
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = "__all__"
